d73514d78469f3214ba4ecf8c3abbc1ef379b5c9
Vio0330 <vio0330@naver.com>
2023-12-05 13:57:48 +0900

    first commit

--- src/ClickCounter.tsx
+++ src/ClickCounter.tsx
@@ -17,7 +17,7 @@ const ClickCounter: React.FC<{ category: string }> = ({ category }) => {
 
     return () => unsubscribe(); // 클린업 함수
   }, [category]);
-
+  //Vio
   const handleClick = () => {
     // Firebase에서 카운트 업데이트
     const countRef = ref(database, `counts/${category}`);
